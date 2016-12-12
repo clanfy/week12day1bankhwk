6a6cc5d929f6b01289702be337d202f556504d7e
Clacson <cookiellacson@gmail.com>
2016-12-12 15:36:20 +0000

    unfactored code removed, refactored code remains

--- client/src/app.js
+++ client/src/app.js
@@ -32,25 +32,6 @@ window.onload = function() {
    accountList.appendChild(accountListItem);
  }
 
-//  var businessTotalDisplay = document.getElementById('business-total');
-//  businessTotalDisplay.innerText = "Business Accounts Total: £" + bank.totalCash("business");
-
-//  var businessAccountList = document.getElementById('business-accounts');
-//  for (var businessAccount of bank.filteredAccounts("business")){
-//   var businessAccountListItem = document.createElement('li');
-//   businessAccountListItem.innerText = businessAccount.owner + ": £" + businessAccount.amount.toFixed(2);
-//   businessAccountList.appendChild(businessAccountListItem);
-// }
-
-// var personalTotalDisplay = document.getElementById('personal-total');
-// personalTotalDisplay.innerText = "Personal Accounts Total: £" + bank.totalCash("personal").toFixed(2); 
-
-// var personalAccountsList = document.getElementById('personal-accounts');
-// for (var personalAccount of bank.filteredAccounts("personal")){
-//   var personalAccountListItem = document.createElement('li');
-//   personalAccountListItem.innerText = personalAccount.owner + ": £" + personalAccount.amount.toFixed(2);
-//   personalAccountsList.appendChild(personalAccountListItem);
-// }
 
 var businessTotalDisplay = document.getElementById('business-total');
 var personalTotalDisplay = document.getElementById('personal-total');
