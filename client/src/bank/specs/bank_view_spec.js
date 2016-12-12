5989f838626accd3c787ca46cfeb235ccfde71fe
Clacson <cookiellacson@gmail.com>
2016-12-12 18:45:38 +0000

    9 tests passed, bank view can add account

--- client/src/bank/specs/bank_view_spec.js
+++ client/src/bank/specs/bank_view_spec.js
@@ -1,11 +1,20 @@
-// var BankView = require('../views/bankView');
-// var Bank = require('../bank');
-// var Account = require('../account');
-// var assert = require('assert');
+var BankView = require('../../views/bankView');
+var Bank = require('../bank');
+var Account = require('../account');
+var assert = require('assert');
 
-//   describe('bank view', function(){
-//     it('should start off with no bank accounts', function(){
-//       var bankView = new BankView();
-//       assert.equal(0, bankView.bank.accounts.length);
-//     });
-//   });
+  describe('bank view', function(){
+    it('should start off with no bank accounts', function(){
+      var bank = new Bank();
+      var bankView = new BankView(bank);
+      assert.equal(0, bankView.bank.accounts.length);
+    });
+
+    it('should ba able to add account', function(){
+      var bank = new Bank();
+      var bankView = new BankView(bank);
+      var account = new Account({owner: 'Cookie', amount: 100, type: 'personal'});
+      bank.addAccount(account);
+      assert.deepEqual(account, bankView.bank.accounts[0]);
+    });
+  });
